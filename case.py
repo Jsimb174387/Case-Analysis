ee189d8d070b9c2d80bf68d200b0c7c9bd2e97e7
James Simbolon <94491116+Jsimb174387@users.noreply.github.com>
2022-11-19 20:01:36 -0500

    Add files via upload

--- case.py
+++ case.py
@@ -1,21 +1,51 @@
 import random
 import numpy as np
+from weapon import skin
 class case:
     def __init__(self):
         #case skin rarity tiers for basic cs:go cases. They have proper names, but these address them by color
         self.rarities = ['blue','purple','pink','red','yellow']
 
+        #skin names in each rarity tier
+        self.blue = ['Jeff', 'Hee']
+        self.purple = []
+        self.pink = []
+        self.red = []
+        self.yellow = []
+        
+
         #2 yellow per 5 red, 1 red per 5 pinks, 1 pink per 5 purples, 1 purple per 5 blues.
         self.odds = [625/782, 125/782, 25/782, 5/782, 2/782]
-
-        #Normal range that the wear value of the skin can be. This is very skin dependent.
-        self.wear_range = [0,1]
-
 
     def simRarity(self, amount):
         return random.choices(self.rarities, self.odds, k = amount)
 
-    
-fal = case()
-print(fal.simRarity(25))
-print(fal.simFloat())
+    def simCaseOpens(self,amount):
+        rarities = self.simRarity(amount)
+        #simulated inventory holds the "skins" unboxed.
+        simInventory = []
+
+
+        for rarity in rarities:
+            if rarity == 'blue':
+                name = random.choices(self.blue)
+                #smin_wear and max_wear values temporary
+                min_wear = 0
+                max_wear = 1
+                newSkin = skin(name, rarity, min_wear, max_wear)
+                simInventory.append(newSkin)
+            elif rarity == 'purple':
+                pass
+            elif rarity == 'pink':
+                pass
+            elif rarity == 'red':
+                pass
+            elif rarity == 'yellow':
+                pass
+
+            
+        return simInventory
+
+phantom = case()
+print(phantom.simCaseOpens(10))
+
--- weapon.py
+++ weapon.py
@@ -1,12 +1,13 @@
 import numpy as np
-class weapon
+class skin:
     def __init__(self, name, rarity, min_wear, max_wear):
-        self.name = name
+        #name is inputted as a array.
+        self.name = name[0]
         self.rarity = rarity
         self.min_wear = min_wear
         self.max_wear = max_wear
 
-    def wearName(wear):
+    def wearName(self, wear):
         #FN: 0 - 0.07, MW: 0.07 - 0.15, FT: 0.15 - 0.38, WW: 0.38 - 0.45, BS: 0.45 - 1
         #Float values are NOT uniform in creation chance! As far as we can tell:
         # https://blog.csgofloat.com/analysis-of-float-value-and-paint-seed-distribution-in-cs-go/
@@ -31,3 +32,6 @@ class weapon
         #obtained from csgofloat.com
         final_float = full_range_float * (self.max_wear - self.min_wear) + self.min_wear
         return final_float
+
+    def __repr__(self):
+        return self.name
