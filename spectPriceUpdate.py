006f1aa6d8e6fed1122e1d51ef0d3876485afca4
Gavin-Sears <112534115+Gavin-Sears@users.noreply.github.com>
2022-12-18 00:26:01 -0500

    Add files via upload
    Fixing multiplying skins bug, even though we don't really need to be using this file.

--- spectPriceUpdate.py
+++ spectPriceUpdate.py
@@ -4,35 +4,42 @@ from time import sleep
 import csv
 
 def unique_to_every(ufile, efile, newfile):
-    with open(ufile, mode='r') as csvfile:
-        csvLines = []
-        csvfile = csv.reader(csvfile)
-        for lines in csvfile:
-            csvLines.append(lines)
+    with open(ufile, mode='r') as csvfile1:
+        #Lines of ufile
+        csvLines1 = []
+        csvFile = csv.reader(csvfile1)
+        for lines in csvFile:
+            csvLines1.append(lines)
 
         price_lines = []
-        applines = csvLines[1:]
-        for line in applines:
+        applines1 = csvLines1[1:]
+        for line in applines1:
                 price_lines.append(line)
 
-    with open(efile, mode='r') as csvfile:
-        csvLines = []
-        csvfile = csv.reader(csvfile)
-        for lines in csvfile:
-            csvLines.append(lines)
+        #price_lines are the lines of the first (unique) file
+
+    with open(efile, mode='r') as csvfile2:
+        #Lines of efile
+        csvLines2 = []
+        csvFile = csv.reader(csvfile2)
+        for lines in csvFile:
+            csvLines2.append(lines)
 
         name_lines = []
-        applines = csvLines[1:]
-        for line in applines:
+        applines2 = csvLines2[1:]
+        for line in applines2:
             name_lines.append(line)
+
+        #name_lines are the lines of the second (every) file
 
     updates_lines = []
     for names in name_lines:
         for prices in price_lines:
-            updates_lines.append(prices)
+            if names[0] == prices[0]:
+                updates_lines.append(prices)
 
-    with open(newfile, mode='w') as csvfile:
-            filewriter = csv.writer(csvfile, delimiter=',')
+    with open(newfile, mode='w') as csvfile3:
+            filewriter = csv.writer(csvfile3, delimiter=',')
             filewriter.writerow(
                 ['hash_name', 'steam_price', 'floatdb_price'])
 
