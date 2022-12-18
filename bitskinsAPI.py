4d7a34559a502bf4e859c5196d6a9767b5979761
Jsimb174387 <JamesCsimbolon@gmail.com>
2022-12-18 16:02:09 -0600

    update

--- bitskinsAPI.py
+++ bitskinsAPI.py
@@ -10,7 +10,7 @@ import pyotp
 # dict = data.json()[0]
 # print(dict['price'])
 
-def ret_prices():
+def ret_ref_prices():
     #https://bitskins.com/api/v1/get_all_item_prices/?api_key=API_KEY&code=CODE&app_id=APP_ID
     api_key = input('Bitskins API Key: ')
     code = input('security code: ')
@@ -18,7 +18,7 @@ def ret_prices():
     data = requests.get(req)
     market = data.json()
 
-    with open('price_data.json', 'w') as outfile:
+    with open('ref_prices.json', 'w') as outfile:
         json.dump(market, outfile)
 
 def secure_code(secret = None):
@@ -32,4 +32,37 @@ def secure_code(secret = None):
     print(my_token.now())
     return(my_token.now())
 
-ret_prices()
+def get_ref_info(hash):
+    skins = []
+    f = open('ref_prices.json',)
+    market_data = json.load(f)
+    for skin in market_data['prices']:
+        if skin['market_hash_name'] == hash:
+            print(skin)
+            skins.append(skin)
+    print(skins)
+
+def ret_market_data():
+    # https://bitskins.com/api/v1/get_all_item_prices/?api_key=API_KEY&code=CODE&app_id=APP_ID
+    api_key = input('Bitskins API Key: ')
+    code = input('security code: ')
+    req = 'https://bitskins.com/api/v1/get_price_data_for_items_on_sale/?api_key=' + api_key + '&code=' + code
+    data = requests.get(req)
+    market = data.json()
+
+    with open('market_prices.json', 'w') as outfile:
+        json.dump(market, outfile)
+
+def get_market_info(hash):
+    skins = []
+    f = open('market_prices.json',)
+    market_data = json.load(f)
+    for skin in market_data['data']['items']:
+        if skin['market_hash_name'] == hash:
+            return [skin['lowest_price'], skin['total_items']]
+    return ['item not found','item not found']
+
+
+#get_market_info('AK-47 | Uncharted (Field-Tested)')
+# f= get_market_info('AK-47 | Uncharted (Field-Tested)')
+# print(f)
