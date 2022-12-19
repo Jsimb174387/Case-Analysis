31738529442530d8c3966f7d1bd2dac9d6a9c164
Gavin-Sears <112534115+Gavin-Sears@users.noreply.github.com>
2022-12-19 00:59:26 -0600

    Add files via upload
    Graphing file. Includes a heat map

--- heatmap.py
+++ heatmap.py
@@ -7,23 +7,20 @@ import statsmodels
 sns.set(font_scale=0.5)
 
 df = pd.read_csv("DSFinalDB")
-df['case_price'] = df['case_price'].str.replace(',', '')
-df['case_price'] = df['case_price'].str.replace('$', '')
-df['case_volume'] = df['case_volume'].str.replace(',', '')
-df['case_volume'] = df['case_volume'].str.replace('"', '')
-df['case_price'] = df['case_price'].astype(float)
-df['case_volume'] = df['case_volume'].astype(int)
+df['price'] = pd.to_numeric(df['price'], errors='coerce')
+df['volume'] = pd.to_numeric(df['volume'], errors='coerce')
+df['case_volume'] = pd.to_numeric(df['case_volume'], errors='coerce')
+df = df.dropna()
+df['case_price'] = df['case_price'].astype(np.float32)
+df['case_volume'] = df['case_volume'].astype(np.int32)
 df['case_name'] = df['case_name'].astype(str)
 df['hash_name'] = df['hash_name'].astype(str)
 df['name'] = df['name'].astype(str)
 df['wear'] = df['wear'].astype(str)
-df['volume'] = df['volume'].astype(int)
-df['price'] = pd.to_numeric(df['price'], errors='coerce')
-df = df.dropna()
-df.info(memory_usage = "deep")
+df['volume'] = df['volume'].astype(np.int32)
 
 #print(df.head()) #show first few rows    
-
+"""
 heatmap = sns.heatmap(df.corr(),
                       vmin=-1,
                       vmax=1,
@@ -33,17 +30,86 @@ heatmap = sns.heatmap(df.corr(),
                       )
 
 plt.savefig("FinalDBheatmap.svg")
+"""
+sns.set(font_scale=1)
+"""
+df = df.drop('case_volume', axis=1)
+df = df.drop('volume', axis=1)
+df = df.drop('wear', axis=1)
+df = df.drop('name', axis=1)
+df = df.drop('hash_name', axis=1)
+df_empty = pd.DataFrame({'price_max' : []})
+df_empty['price_max'] = df.groupby(['case_name', 'case_price'])['price'].transform(max)
+df = df.drop('price', axis=1)
+df['price_max'] = df_empty['price_max']
+df = df.drop_duplicates()
 
-plt.subplots(figsize = (10, 10))
-
-
-cor = sns.lmplot(x ="case_price",
-                 y ="price",
-                 hue ="case_name",
-                 data = df,
+cor = sns.lmplot(data = df,
+                 x = 'case_price',
+                 y = 'price_max',
+                 hue = 'case_name',
                  order = 1,
-                 logistic=False,
-                 y_jitter=.03)
+                 logistic = False,
+                 y_jitter=0.03)
 
+fig = cor.figure
 
-cor.savefig("FinalDBPlot.svg")
+fig.savefig('cormax.svg')
+"""
+
+df = df.drop('case_volume', axis=1)
+df = df.drop('volume', axis=1)
+df = df.drop('wear', axis=1)
+df = df.drop('name', axis=1)
+df = df.drop('hash_name', axis=1)
+df_empty = pd.DataFrame({'price_avg' : []})
+df_empty['price_avg'] = df.groupby(['case_name', 'case_price'])['price'].transform(np.mean)
+df = df.drop('price', axis=1)
+df['price_avg'] = df_empty['price_avg']
+df = df.drop_duplicates()
+
+cor = sns.lmplot(data = df,
+                 x = 'case_price',
+                 y = 'price_avg',
+                 hue = 'case_name',
+                 order = 1,
+                 logistic = False,
+                 y_jitter=0.03)
+
+fig = cor.figure
+
+fig.savefig('coravg.svg')
+
+"""
+cases = ['chroma', 'chroma2', 'chroma3', 'danger', 'horizon', 'prisma', 'prisma2', 'spect', 's2']
+
+sns.set(font_scale=1)
+
+for case in cases:
+
+    df = pd.read_csv(case + "final.csv")
+    df['price'] = pd.to_numeric(df['price'], errors='coerce')
+    df['volume'] = pd.to_numeric(df['volume'], errors='coerce')
+    df['case_volume'] = pd.to_numeric(df['case_volume'], errors='coerce')
+    df = df.dropna()
+    df['case_price'] = df['case_price'].astype(np.float32)
+    df['case_volume'] = df['case_volume'].astype(np.int32)
+    df['case_name'] = df['case_name'].astype(str)
+    df['hash_name'] = df['hash_name'].astype(str)
+    df['name'] = df['name'].astype(str)
+    df['wear'] = df['wear'].astype(str)
+    df['volume'] = df['volume'].astype(np.int32)
+    df = df.head(300)
+
+    plt.subplots(figsize = (10, 10))
+
+    sca = sns.scatterplot(x ="price",
+                     y ="name",
+                     hue ="wear",
+                     data = df
+                     )
+
+    fig = sca.figure
+
+    fig.savefig(case + "FinalDBPlot.svg")
+    """
